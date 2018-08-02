d170286f579afa26cfc57dab83143cebbb5cf727
Dipesh <bdipesh08@gmail.com>
2018-08-02 10:24:43 +0545

    4 app created

---
+++ VUE/app.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el:'#first',
+  data:{
+    message: 'You loaded' + new Date().toLocaleString()
+  }
+});
+var app2 = new Vue({
+  el:'#second',
+  data:{
+    seen: true
+  }
+});
+var app3 = new Vue({
+  el:'#third',
+  data:{
+    todos:[
+      {text:'Learn js'},
+      {text: 'Learn Vue'},
+      {text: 'Build something new'}
+    ]
+  }
+});
+var app4 =  new Vue({
+  el:'#four',
+  data:{
+    message:'you are awesome'
+  },
+  methods:{
+    reverseMessage: function(){
+      this.message = this.message.split('').reverse().join('');
+    }
+  }
+});
